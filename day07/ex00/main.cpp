d8d82a17659bf3fac95ba50fcd98bed225a3081f
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-02-25 11:24:03 +0000

    add main

--- day07/ex00/main.cpp
+++ day07/ex00/main.cpp
@@ -6,21 +6,25 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 11:10:44 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/02/25 11:21:13 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/02/25 11:23:18 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "whatever.hpp"
 
 
-int main()
-{
+int main( void ) {
 	int a = 2;
 	int b = 3;
-
-	swap<int>(a,b);
-	std::cout<< "a = "<< a << " b = "<< b << std::endl;
-	std::cout<< "min = "<< min<int>(a,b)<< std::endl;
-	std::cout<< "max = "<< max<int>(a,b)<< std::endl;
-	
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
 }
