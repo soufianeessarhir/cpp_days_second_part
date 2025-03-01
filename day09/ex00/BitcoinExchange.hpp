87bf728bdd4b1827e711c5b14c3583729f11819e
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-03-01 19:45:37 +0000

    starting day09

--- day09/ex00/BitcoinExchange.hpp
+++ day09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:35:26 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/02/28 15:36:49 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/03/01 18:25:50 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,26 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 #include <iostream>
+#include <map>
 
+class BitcoinExchange
+{
+private:
+	std::map<std::string, double> data;
+public:
+	BitcoinExchange();
+	~BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange&);
+	BitcoinExchange & operator=(const BitcoinExchange&);
+	void processipute(std::string str);
+};
 
+BitcoinExchange::BitcoinExchange()
+{
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
 
 #endif
--- day09/ex00/main.cpp
+++ day09/ex00/main.cpp
@@ -6,7 +6,15 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:35:22 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/02/28 15:35:23 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/03/01 17:11:10 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char *av)
+{
+	if (ac != 2)
+		return std::cout << "Error: could not open file.\n",1;
+	
+}
