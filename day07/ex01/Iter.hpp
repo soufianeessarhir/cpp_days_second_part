d80a258ac2a4bdff38ca1d07aec1ce8ac991f620
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-03-01 11:26:57 +0000

    starting day09

--- day07/ex01/Iter.hpp
+++ day07/ex01/Iter.hpp
@@ -15,14 +15,13 @@
 #define ITER_HPP
 
 #include <iostream>
-template <typename T, typename FUNC>
-
-void iter(T* arr, int length, FUNC func)
+template <typename T, typename Func>
+void iter(T *array, size_t length, Func function)
 {
-	for (int i = 0; i < length ;++i)
-	{
-		func(arr[i]);
-	}
+    for (size_t i = 0; i < length; i++)
+    {
+        function(array[i]);
+    }
 }
 
 #endif
