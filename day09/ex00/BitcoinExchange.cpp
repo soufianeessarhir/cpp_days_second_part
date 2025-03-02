8705f9d5a8ef76ed1399b7a0d3009f9c2fede63c
Soufiane Essarhir <soufianeessarhir@gmail.com>
2025-03-02 12:01:21 +0000

    start parsing the input

--- day09/ex00/BitcoinExchange.cpp
+++ day09/ex00/BitcoinExchange.cpp
@@ -6,13 +6,15 @@
 /*   By: sessarhi <sessarhi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:36:24 by sessarhi          #+#    #+#             */
-/*   Updated: 2025/03/02 10:54:55 by sessarhi         ###   ########.fr       */
+/*   Updated: 2025/03/02 11:59:28 by sessarhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 #include <fstream>
 #include <sstream>
+#include <errno.h>
+#include <cmath>
 
 BitcoinExchange::BitcoinExchange()
 {
@@ -22,7 +24,7 @@ BitcoinExchange::BitcoinExchange()
 	char dash,dash1,comma;
 	double value;
 	uint_64 key;
-	if(file)
+	if(file.is_open())
 	{
 		while(getline(file,tmp))
 		{
@@ -33,7 +35,7 @@ BitcoinExchange::BitcoinExchange()
 		}
 	}
 	else
-		std::cout<<"can't open data file";
+		std::cerr<<"can't open data file";
 	
 }
 
@@ -56,4 +58,43 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& rhs)
 void BitcoinExchange::processipute(std::string FileName)
 {
 	std::ifstream file(FileName.c_str());
+	std::string tmp;
+	int year,month,day;
+	char dash,dash1,del;
+	std::string strval;
+	double value;
+	// uint_64 key;
+	if(file.is_open())
+	{
+		while(getline(file,tmp))
+		{
+			std::stringstream os(tmp);
+			if (os>>year>>dash>>month>>dash1>>day>>del>>strval)
+			{
+				if (dash == '-' || dash1 == '-' || del == '|')
+				{
+					errno = 0;
+					char *end = NULL;
+					value = strtod(strval.c_str(),&end);
+					if (*end || std::isinf(value) || std::isnan(value))
+						std::cout << "bad input\n";
+					else if (errno || value > 1000)
+						std::cerr<<"too large a numbe\n";
+					else if (value < 0)
+						std::cout << "not a positive number.\n";
+					else
+					{
+						std::cout<< "ok\n";
+					}
+				}
+				else
+					std::cout << "bad input\n";
+			}
+			else
+				std::cout << "bad input\n";
+		}
+	}
+	else
+		std::cerr<<"can't open data file";
+	
 }
